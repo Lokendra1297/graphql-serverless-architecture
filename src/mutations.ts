865a52f5ccbb65bc5fec5ae54ae5bc14e30c3439
Lokendra <lokendrasr12@gmail.com>
2024-09-30 10:50:12 +0530

    Updated mutation, query, and subscription handlers to remove comments and add CORS headers

--- src/mutations.ts
+++ src/mutations.ts
@@ -15,11 +15,19 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
     const response = await axios.post(process.env.GRAPHQL_API, { query: mutation });
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "http://mywebsite-graphql.s3-website.ap-south-1.amazonaws.com",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
       body: JSON.stringify(response.data),
     };
   } catch (error) {
     return {
       statusCode: 500,
+      headers: {
+        "Access-Control-Allow-Origin": "http://mywebsite-graphql.s3-website.ap-south-1.amazonaws.com",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
       body: JSON.stringify({ error: error.message }),
     };
   }
